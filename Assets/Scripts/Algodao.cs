38fb87a0e69089e4b1f844c4441e926d05301046
Roger Castellar <rogerpcto@yahoo.com.br>
2024-12-27 21:07:21 -0300

    Mecânica de molhar algodão
    - Algodão muda de cor ao ficar molhado na banheira
    - Ajustes que afetavam a física do algodão

--- Assets/Scripts/Algodao.cs
+++ Assets/Scripts/Algodao.cs
@@ -4,33 +4,51 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Algodao : MonoBehaviour
 {
     private XRGrabInteractable _grabInteractable;
+    private Renderer _renderer;
     private bool _isHold;
+    private bool _isWet;
+
 
     [SerializeField]
     private float _destroyDelay = 2.0f;
 
-    private XRGrabInteractable grabInteractable;
-
     private void Awake()
     {
-        grabInteractable = GetComponent<XRGrabInteractable>();
+        _grabInteractable = GetComponent<XRGrabInteractable>();
+        _renderer = GetComponent<Renderer>();
 
-        if (grabInteractable != null)
+        if (_grabInteractable != null)
         {
-            grabInteractable.selectExited.AddListener((args) =>
+            _grabInteractable.selectExited.AddListener((args) =>
             {
                 Invoke(nameof(SelfDestroy), _destroyDelay);
             });
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Agua") && !_isWet)
+        {
+            BecomeWet();
+        }
+    }
+
+    private void BecomeWet()
+    {
+        _isWet = true;
+        SetBaseColor(new Color(120 / 255f, 140 / 255f, 160 / 255f));
+    }
+
+    private void SetBaseColor(Color color) => _renderer.material.color = color;
+
     private void SelfDestroy() => Destroy(gameObject);
 
     private void OnDestroy()
     {
-        if (grabInteractable != null)
+        if (_grabInteractable != null)
         {
-            grabInteractable.selectExited.RemoveAllListeners();
+            _grabInteractable.selectExited.RemoveAllListeners();
         }
     }
 }
