9f1e4593603e566dea34d50cd1358a13bd7e6b2f
Roger Castellar <rogerpcto@yahoo.com.br>
2025-01-29 00:47:12 -0300

    Sistema de remoção de espuma

---
+++ Assets/Scripts/EspumaRemover.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EspumaRemover : MonoBehaviour
+{
+    private const float TAXA_REMOCAO = 0.1f;
+
+    [SerializeField]
+    private ParticleSystem _espumaParticleSystem;
+    [SerializeField]
+    private Bebe _bebe;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        var emission = _espumaParticleSystem.emission;
+        var rate = emission.rateOverTime;
+
+        if (rate.constant <= 0)
+            return;
+
+        float taxaAtual = rate.constant;
+        float taxaNova = Mathf.Clamp(taxaAtual - TAXA_REMOCAO, 0f, float.MaxValue);
+
+        rate.constant = taxaNova;
+        emission.rateOverTime = rate;
+
+        if (taxaNova <= 0)
+        {
+            _bebe.Sorrir();
+        }
+    }
+}
